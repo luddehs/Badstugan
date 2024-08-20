f2cc3ecd8df7e5ae2faa66482fbad3a1853379cf
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-08-20 19:04:32 +0000

    Remove import from product model

--- products/models.py
+++ products/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    duration = models.DurationField()  # e.g., 1 hour, 2 hours
+    duration = models.DurationField()  # Duration of the booking, e.g., 2 hours
     is_private = models.BooleanField(default=False)
     requires_membership = models.BooleanField(default=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
