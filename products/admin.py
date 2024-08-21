3d9d6862576049ffb26a2b296a477c275cac4e54
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-08-21 06:45:49 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'price',
+        'duration',
+        'is_private',
+        'requires_membership',
+        'image',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Product, ProductAdmin)
