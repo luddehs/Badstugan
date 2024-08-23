881c6b4ddb62e14b0541bf2ff7db98a73979e298
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-08-23 10:03:20 +0000

    Create and update files for deployment

--- badstugan/settings.py
+++ badstugan/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3e-&h40pua4h9)5fuz^7#28+mi!kdd8z*d_didasftdc=gk^0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-luddehs-badstugan-0uhxvemzzm3.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-luddehs-badstugan-0uhxvemzzm3.ws.codeinstitute-ide.net','.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['http://8000-luddehs-badstugan-0uhxvemzzm3.ws.codeinstitute-ide.net', 'https://8000-luddehs-badstugan-0uhxvemzzm3.ws.codeinstitute-ide.net']
 
