733975b3f36545f37da92037c04b5805e647a4a9
luddehs <ludvig.hs@gmail.com>
2024-11-14 15:50:36 +0100

    Add category filtering with centered product layout

--- products/views.py
+++ products/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
-    """ A view to show all products """
+    """ A view to show all products with optional category filtering """
     products = Product.objects.all()
+    categories = Category.objects.all()
+    
+    # Get category from URL parameter
+    category = request.GET.get('category')
+    if category:
+        products = products.filter(category__name=category)
+
     context = {
         'products': products,
+        'categories': categories,
+        'current_category': category,
     }
     return render(request, 'products/products.html', context)
 
