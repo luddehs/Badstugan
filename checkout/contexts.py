80f296a6b9fdca54894fdf1d4dfa8f04795739ac
luddehs <ludvig.hs@gmail.com>
2024-11-15 11:39:19 +0100

    Add checkout context

---
+++ checkout/contexts.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+from django.conf import settings
+
+def checkout_contents(request):
+
+    checkout_items = []
+    total = 0
+    product_count = 0
+    grand_total = total
+    
+    context = {
+        'checkout_items': checkout_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
