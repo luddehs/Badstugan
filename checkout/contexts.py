f3a67d705cc23df3f6972cd31e86e0ee5ccd580d
luddehs <ludvig.hs@gmail.com>
2024-12-16 09:15:25 +0100

    Add booking details display with date, time slot and guest information to checkout page

--- checkout/contexts.py
+++ checkout/contexts.py
@@ -1,13 +1,31 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product, TimeSlot
 
 def checkout_contents(request):
-
     checkout_items = []
     total = 0
     product_count = 0
+    checkout = request.session.get('checkout', {})
+
+    for item_id, item_data in checkout.items():
+        if isinstance(item_data, dict):
+            product = get_object_or_404(Product, pk=item_id)
+            time_slot = get_object_or_404(TimeSlot, pk=item_data['time_slot'])
+            total += item_data['quantity'] * product.price
+            product_count += item_data['quantity']
+            checkout_items.append({
+                'item_id': item_id,
+                'quantity': item_data['quantity'],
+                'product': product,
+                'time_slot': time_slot,
+                'booking_date': item_data['booking_date'],
+                'subtotal': item_data['quantity'] * product.price
+            })
+
     grand_total = total
-    
+
     context = {
         'checkout_items': checkout_items,
         'total': total,
--- checkout/views.py
+++ checkout/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render, redirect
-
-
-# Create your views here.
+from django.contrib import messages
 
 def checkout(request):
     """ A view to return the checkout page """
-
+    checkout = request.session.get('checkout', {})
     
+    if not checkout:
+        messages.error(request, "There's nothing in your checkout at the moment")
+        return redirect('products')
+        
     return render(request, 'checkout/checkout.html')
 
 def add_to_checkout(request, item_id):
     """ Add a quantity of the specified product to the checkout """
 
     quantity = int(request.POST.get('quantity'))
+    time_slot = request.POST.get('time_slot')
+    booking_date = request.POST.get('booking_date')
     redirect_url = request.POST.get('redirect_url')
     checkout = request.session.get('checkout', {})
 
-    if item_id in list(checkout.keys()):
-        checkout[item_id] += quantity
-    else:
-        checkout[item_id] = quantity
+    checkout[item_id] = {
+        'quantity': quantity,
+        'time_slot': time_slot,
+        'booking_date': booking_date
+    }
 
     request.session['checkout'] = checkout
-    print(request.session['checkout'])
     return redirect(redirect_url)
